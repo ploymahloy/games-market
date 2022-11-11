ff3e232e9776394a25d02e22d0d63724bd7efb88
Patrick Mahloy <patrick.mahloy@gmail.com>
2022-11-11 12:14:35 -0500

    Debugged TailwindCSS install, removed hacky cdn imports

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    concurrentFeatures: false,
+    serverComponents: true
+  }
 }
 
 module.exports = nextConfig
